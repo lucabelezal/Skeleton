6a5dfb40e0d5b8555be4f17640812a9f4b531ca5
lucabelezal <lucabelezal@gmail.com>
2019-06-29 21:30:04 -0300

    Add project

---
+++ Skeleton/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Skeleton
+//
+//  Created by Lucas Nascimento on 29/06/19.
+//  Copyright © 2019 Lucas Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
