f557c87bd035a3631c80f175ada14e92eaf382cd
Lucas Nascimento <leddnascimento@gmail.com>
2020-11-19 12:15:28 -0300

    Update pbxproj

--- SkeletonTests/SkeletonTests.swift
+++ SkeletonTests/SkeletonTests.swift
@@ -2,32 +2,32 @@
 //  SkeletonTests.swift
 //  SkeletonTests
 //
-//  Created by Lucas Nascimento on 29/06/19.
-//  Copyright © 2019 Lucas Nascimento. All rights reserved.
+//  Created by l.b.do.nascimento on 19/11/20.
+//  Copyright © 2020 Lucas Nascimento. All rights reserved.
 //
 
 import XCTest
-@testable import Skeleton
 
 class SkeletonTests: XCTestCase {
 
-    override func setUp() {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
+    func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
+
 }
